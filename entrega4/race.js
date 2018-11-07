d27505b5e7a3631e78b646cbd7078443df67a226
Samanta Torrez <dsamantatorrez@gmail.com>
2018-11-06 22:11:52 -0300

    Entrega 4

---
+++ entrega4/race.js
@@ -0,0 +1,341 @@
+$( document ).ready(function() {
+    const CONTAINER= "container";
+    const CANT_PIXELS_MOV_SIDE = 5;
+    const CANT_PIXELS_MOV_TOP = 3;
+    const CANT_ENEMIES = 3;
+    const BORDER_LEFT = 5;
+    const BORDER_RIGHT = $("#container").width()-5;
+    const BONUS = 10;
+    var music = 'dont-stop-me-now-8-bit-tribute-to-queen-8-bit-universe.mp3';
+    var crash = 'crash.mp3';
+    var audio;
+    var enemyShowed = null;
+    var starShowed = null;
+    var borderEvent = false;
+    var keys = {};
+    var spamEnemies1;
+    var spamStars1;
+    var puntaje;
+    var counter;
+    var isRestart = false;
+
+    class Piece {
+        constructor (id) {
+            this.id = id;
+            this.updatePosition();
+        }
+        getId(){
+            return "#"+this.id;
+        }
+        getPosition () {
+            return $( this.getId() ).position();
+        }
+        getWidht() {
+            return $( this.getId() ).width();
+        }
+        getHeight() {
+            return $( this.getId() ).height();
+        }
+        updatePosition() {
+            let pos = this.getPosition();
+            this.top = pos.top;
+            this.right =  pos.left + this.getWidht();
+            this.left = pos.left;
+            this.bottom = pos.top + this.getHeight();
+        }
+        
+        collision(piece) {
+            this.updatePosition();
+            borderEvent = this.left <= BORDER_LEFT || this.right >= BORDER_RIGHT;
+
+           return (((this.top<= piece.bottom && piece.bottom <= this.bottom)
+                    || (this.top<= piece.top && piece.top <= this.bottom)) 
+                    &&
+                    ((this.left <= piece.left && piece.left <= this.right)
+                    || (this.left <= piece.right && piece.right <= this.right)))
+                    || borderEvent;
+        }
+    }
+    function resetScore(){
+        puntaje=0;
+        $("#puntuacion").html(puntaje);
+    }
+    function startCounter(){
+        counter=setInterval(function(){
+            puntaje++;
+            $("#puntuacion").html(puntaje);
+          },500);
+    }
+    function stopCounter(){
+        clearInterval(counter);
+    }
+    function addBonus() {
+        puntaje+=BONUS;
+        $("#puntuacion").html(puntaje);
+    }
+    var player = new Piece("car");
+    var enemy1 = new Piece("enemy1");
+    var enemy2 = new Piece("enemy2");
+    var enemy3 = new Piece("enemy3");
+    var star1 = new Piece("star1");
+    var star2 = new Piece("star2");
+    var star3 = new Piece("star3");
+
+    $('#volumen').on("change mousemove", function() {
+        audio.volume =$(this).val();
+        crashEffect.volume =$(this).val();
+    });
+    function hiScores(params) {
+        //to do
+        $('#myModal').modal('show');
+        $('#saveScore').click(function() {
+        $('#myModal').modal('toggle');
+        $('#ranking').modal('toggle');
+      });
+    }
+    
+    /* Metodos de carrera*/
+    $("#start").click(function() {
+        startRace();
+    });
+    function startRace() {
+        refresh();
+        document.addEventListener("keydown", keyDown, false);
+        document.addEventListener("keyup", keyUp, false);
+        $("#car").show();
+        audio = new Audio(music);
+        audio.addEventListener("ended", function(){
+            audio.currentTime = 0;
+       });
+        audio.play();
+        crashEffect = new Audio(crash);
+        spamEnemies();
+        spamStars();
+        checkCollisions();
+        keysRestore();
+        isRestart = false;
+        mover();
+        startCounter();
+    }
+    /*Vuelve los parametros a sus valores iniciales */
+    function refresh(){
+        if(enemyShowed){
+            $("#enemy"+enemyShowed).removeClass("pause");
+            $("#enemy"+enemyShowed).removeClass("rotateImage");
+            $("#enemy"+enemyShowed).removeClass("enemy");
+            $("#enemy"+enemyShowed).hide();
+            enemyShowed=null;
+        }
+        if(starShowed){
+            $("#star"+starShowed).removeClass("pause");
+            $("#star"+starShowed).removeClass("star");
+            $("#star"+starShowed).hide();
+        }
+        resetScore();
+        let carDiv = document.getElementById("car");
+        let carImg = carDiv.getElementsByTagName('img')[0];
+        carImg.src = "car.png";
+        carDiv.style.left="140px";
+        carDiv.style.top="365px";
+
+        resetScore();
+        $("#container").removeClass("pause");
+        $("#container").addClass("carrera");
+        $("#start").hide();
+        $("#car").removeClass("rotateImage");
+    }
+
+    function removeListeners(){
+        document.removeEventListener("keydown", keyDown, false);
+        document.removeEventListener("keyup", keyUp, false);
+    }
+
+    /* EVENTOS de mouse*/
+    function keyUp(event) {
+        keys[event.which] = false;
+    }
+    function keyDown(event) {
+        keys[event.which] = true;
+    }
+    function keysRestore() {
+        keys[37] = false;
+        keys[39] = false;
+        keys[38] = false;
+        keys[40] = false;
+    }
+    function mover() {
+        //left
+        if(keys[37]){
+            moveArrowSide(player.id,-CANT_PIXELS_MOV_SIDE);
+        }
+        //Right
+        if(keys[39]){
+            moveArrowSide(player.id,CANT_PIXELS_MOV_SIDE);
+        }
+        //Up
+        if(keys[38]){
+            moveArrowTop(player.id,-CANT_PIXELS_MOV_TOP);
+        }
+        //Down
+        if(keys[40]){
+            moveArrowTop(player.id,CANT_PIXELS_MOV_TOP);
+        }
+        if(!isRestart){setTimeout(mover, 20);}
+    }
+    function moveArrowSide(id,cant) {
+        let element = document.getElementById(id);
+        let containter= document.getElementById(CONTAINER);
+        let base = element.style.left;
+       if(base=== ""){
+            base = 140;
+        }
+        let sum = parseInt(base)+cant;
+        if(sum >= 0 && sum <= containter.offsetWidth - element.offsetWidth ){
+            element.style.left = sum + 'px';
+        }
+    }
+    function moveArrowTop(id,cant) {
+        let element = document.getElementById(id);
+        let containter= document.getElementById(CONTAINER);
+        let base = element.style.top;
+        if(base=== ""){
+            base = 365;
+        }
+        let sum = parseInt(base)+cant;
+        if(sum >= 0 && sum <= containter.offsetHeight - element.offsetHeight ){
+            element.style.top = sum + 'px';
+        }
+    }
+
+    /*EVENTOS de colisiones*/
+    function spamEnemies() {
+        spamEnemies1 = setInterval(showEnemy, 2000);
+    }
+    function showEnemy() {
+      let i = getRandomInt(1,CANT_ENEMIES);
+      if(enemyShowed){
+        $("#enemy"+enemyShowed).hide();
+        $("#enemy"+enemyShowed).removeClass("enemy");
+      }
+      $("#enemy"+i).show();
+      $("#enemy"+i).addClass("enemy");
+
+      enemyShowed=i;
+    }
+
+    function spamStars() {
+        spamStars1 = setInterval(spamStar, 2000);
+    }
+    function spamStar() {
+      let i = getRandomInt(1,CANT_ENEMIES);
+      if(enemyShowed){
+        while (i==enemyShowed) {
+            i = getRandomInt(1,CANT_ENEMIES);
+        }
+        if(starShowed){
+            $("#star"+starShowed).hide();
+            $("#star"+starShowed).removeClass("star");
+        }
+        $("#star"+i).show();
+        $("#star"+i).addClass("star");
+        starShowed=i;
+      }
+    }
+
+    function checkCollisions() {
+        collisions = setInterval(checkCollision, 100);
+    }
+    function restart(){
+        borderEvent=false;
+        $("#start").show();
+        
+    }
+    function checkCollision() {
+        if (detectCollision()){
+            isRestart=true;
+            removeListeners();
+            stopCounter();
+            if(enemyShowed){
+                $("#enemy"+enemyShowed).addClass("pause");
+            }
+            if(starShowed){
+                $("#star"+starShowed).addClass("pause");
+            }
+            clearInterval(spamEnemies1);
+            clearInterval(spamStars1);
+            clearInterval(collisions);
+            $("#container").addClass("pause");
+            rotateImg(player.id);
+            if (!borderEvent) {
+                rotateImg("enemy"+enemyShowed);
+            }
+            audio.pause();
+            playCrash();
+            restart();
+        } else if (detectBonusCollision()){
+            addBonus();
+            $("#star"+starShowed).hide();
+            $("#star"+starShowed).removeClass("star");
+        }
+    }
+    /*Evento de girar coche */
+    function rotateImg(id){
+        let carDiv = document.getElementById(id);
+        let carImg = carDiv.getElementsByTagName('img')[0];
+        if(id=="car") {
+            carImg.src = "crash.png";
+        }
+        carDiv.classList.add("rotateImage");
+    }
+    /*Evento de sonido */
+    function playCrash() {
+            crashEffect.play();
+            setTimeout(function(){
+                crashEffect.pause();
+            }, 3395.86);
+    }
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    function detectCollision(){
+        let collision = false;
+        switch (enemyShowed) {
+            case 1:
+                enemy1.updatePosition();
+                collision = player.collision(enemy1);
+            break;
+            case 2:
+                enemy2.updatePosition();
+                collision = player.collision(enemy2);
+            break;
+            case 3:
+                enemy3.updatePosition();
+                collision = player.collision(enemy3);
+            break;    
+            default:
+            break;
+      }
+      return collision;
+    }
+    function detectBonusCollision(){
+        let collision = false;
+        switch (starShowed) {
+            case 1:
+                star1.updatePosition();
+                collision = player.collision(star1);
+            break;
+            case 2:
+                star2.updatePosition();
+                collision = player.collision(star2);
+            break;
+            case 3:
+                star3.updatePosition();
+                collision = player.collision(star3);
+            break;    
+            default:
+            break;
+      }
+      return collision;
+    }
+    
+})

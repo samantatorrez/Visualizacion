ea70e371aff71fc2fa89b6a1096935da08ec0521
Samanta Torrez <dsamantatorrez@gmail.com>
2018-12-18 11:35:39 -0300

    primeros archivos final

---
+++ final/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $.ajax({
+        url: "https://my-json-server.typicode.com/samantatorrez/twitter/comments",
+        dataType: 'json',
+        type: 'get',
+        cache:false,
+        success: function(data){
+            var event_data = '';
+            $.each(data, function(index, value){
+                event_data += '<li class="list-group-item align-items-center justify-content-between">';
+                event_data += '<div class="commenterImage"><img id="imgUser" src="assets/img/'+value.img+'" /></div>';
+                event_data += '<div class="commentText"><p>'+value.name+'</p>';
+                event_data += '<p>'+value.body+'</p>';
+                event_data += '<span class="date sub-text">'+ value.date+'</span></div>';
+                event_data += '</li>';
+            });
+            $("#comentarios").append(event_data);
+        },
+        error: function(d){
+            alert("404. Please wait until the File is Loaded.");
+        }
+    });
+    $("#filtrar" ).click(function(){
+        $("#collapsibleNavbar").toggle();
+    });
+});
